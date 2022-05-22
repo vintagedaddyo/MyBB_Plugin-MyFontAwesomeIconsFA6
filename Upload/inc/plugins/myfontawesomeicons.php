3aab8bd3b8ef2b405583dc420f9f89b3bb29f89c
vintagedaddyo <vintagedaddyo@gmail.com>
2022-05-22 03:45:33 -0700

    Update myfontawesomeicons.php

--- Upload/inc/plugins/myfontawesomeicons.php
+++ Upload/inc/plugins/myfontawesomeicons.php
@@ -203,6 +203,12 @@ if(!defined("IN_MYBB"))
 			if($pluginargs['title'] == $lang->display_order)
 			{
 				$lang->load('forum_management_myfontawesomeicons');
+
+            if(empty($forum_data['myfontawesomeicons_icon']))
+            {
+                // display suggested icon in input if empty
+                $forum_data['myfontawesomeicons_icon'] = "fa-solid fa-comments";
+            }                
                 
 				$form_container->output_row($lang->myfontawesomeicons_forum_icons, $lang->myfontawesomeicons_forum_icons_desc, $form->generate_text_box('myfontawesomeicons_icon', $forum_data['myfontawesomeicons_icon'], array('id' => 'myfontawesomeicons_icon')));
 			}
